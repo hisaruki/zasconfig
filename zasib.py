c83b92370fe896175cebb05f36dcad18e784d3c5
hisaruki <hisaruki@morningnegi.ddo.jp>
2016-12-26 18:37:04 +0900

    バックアップ元に対象スナップショットが存在しない場合に対応

--- zasib.py
+++ zasib.py
@@ -38,24 +38,25 @@ class Zasib:
 
   def send(self):
     send,recv = None,None
-    if len(self.listto) == 0:
-      send = ["zfs","send","-R",self.listfrom[-1]]
-      if type(self.prefrom) == list:
-        send = self.prefrom + send
-      recv = ["zfs","recv",self.nto]
-      if type(self.preto) == list:
-        recv = self.preto + recv
-    else:
-      tk = self.listto[-1].split("@")[-1]
-      fks = list(map(lambda x:x.split("@")[-1],self.listfrom))
-      fk = fks[fks.index(tk)]
-      if self.nfrom+"@"+fk != self.listfrom[-1]:
-        send = ["zfs","send","-I",self.nfrom+"@"+fk,self.listfrom[-1]]
+    if len(self.listfrom) > 0:
+      if len(self.listto) == 0:
+        send = ["zfs","send","-R",self.listfrom[-1]]
         if type(self.prefrom) == list:
           send = self.prefrom + send
         recv = ["zfs","recv",self.nto]
         if type(self.preto) == list:
           recv = self.preto + recv
+      else:
+        tk = self.listto[-1].split("@")[-1]
+        fks = list(map(lambda x:x.split("@")[-1],self.listfrom))
+        fk = fks[fks.index(tk)]
+        if self.nfrom+"@"+fk != self.listfrom[-1]:
+          send = ["zfs","send","-I",self.nfrom+"@"+fk,self.listfrom[-1]]
+          if type(self.prefrom) == list:
+            send = self.prefrom + send
+          recv = ["zfs","recv",self.nto]
+          if type(self.preto) == list:
+            recv = self.preto + recv
 
     if send and recv:
       sys.stdout.write(" ".join(send)+" | "+" ".join(recv))
@@ -74,5 +75,4 @@ class Zasib:
 
 z = Zasib(args.nfrom,args.nto,args.prefrom,args.preto)
 z.send()
-#z.send()
 #./zasib.py pride/test zroot/test --prefrom sudo --preto ssh greed sudo
