6ad9fd6459ec9904dac09de33d8f8323448693e0
hisaruki <hisaruki@gmail.com>
2020-04-11 18:47:57 +0900

    debug

--- zasconfig.py
+++ zasconfig.py
@@ -1,93 +1,157 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import subprocess
+from subprocess import Popen, PIPE
 import sys
-import collections
+import re
+import os
+import datetime
 import argparse
 
-parser = argparse.ArgumentParser(description="ZFS Auto Snapshot Config")
-parser.add_argument('--mode', default="view")
-parser.add_argument('--reset', dest="mode",
-                    action="store_const", const="reset")
-parser.add_argument('--on', dest="mode", action="store_const", const="on")
-parser.add_argument('--off', dest="mode", action="store_const", const="off")
-parser.add_argument('--levels')
-parser.add_argument('target', nargs="?")
-args = parser.parse_args()
+class Snapshot:
+    def __init__(self, l, dataset):
+        self.dataset = dataset
+        l = l.split()
+        self.name = l[0]
+        self.name = self.name.split("@")[-1]
+        self.fullname = self.dataset.name + "@" + self.name
+        self.used = l[1]
+        self.avail = l[2]
+        self.refer = l[3]
+        self.mountpoint = l[4]
 
+    def rename(self, newname=None):
+        if newname is None:
+            newname = re.sub(r'[^0-9]', '', self.name)[0:8]
+        newfullname = "{}@{}".format(self.dataset.name, newname)
 
-def view():
-    if len(status) > 0:
-        i = 0
-        for attr in attrs:
-            key = "com.sun:auto-snapshot:" + attr
-            proc = subprocess.Popen([
+        if not self.dataset.search_snapshot(newname):
+            sys.stderr.write("# {} => {}\n".format(self.fullname, newfullname))
+            proc = [
                 "zfs",
-                "get",
-                key,
-            ], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-            for line in proc[0].decode("utf-8").splitlines():
-                if line.split()[2] == "true" and line.split()[0] in status:
-                    status[line.split()[0]][i] = "1"
-            i += 1
-    for path in status:
-        line = list(map(lambda x: x.rjust(8), status[path]))
-        print(path.ljust(max([len(x) for x in status])) + "".join(line))
+                "rename",
+                self.fullname,
+                newname
+            ]
+            proc = self.dataset.prefix + proc
+            sys.stderr.write(" ".join(proc) + "\n")
+            Popen(proc).communicate()
+            self.dataset.create_status()
+    def destroy(self):
+        proc = [
+            "zfs",
+            "destroy",
+            self.fullname
+        ]
+        proc = self.dataset.prefix + proc
+        sys.stderr.write(" ".join(proc) + "\n")
+        Popen(proc).communicate()
+        self.dataset.create_status()
+
+class Zfs:
+    def __init__(self, datasetname):
+        self.name = datasetname
+        self._snapshots = []
+        self.prefix = []
+        self.create_status()
+
+    def create_status(self):
+        proc = ["zfs", "list", self.name, "-t", "snapshot"]
+        o, e = Popen(proc, stdout=PIPE, stderr=PIPE).communicate()
+        self.exists = True
+        if e:
+            self.exists = False
+        for l in o.decode().splitlines():
+            if l.find("NAME") == 0:
+                continue
+            snapshot = Snapshot(l, self)
+            self._snapshots.append(snapshot)
+
+    def _filter_snapshots(self, mode):
+        f = self._snapshots
+        if mode == "znap":
+            f = filter(lambda x:x.name.find("znap") >= 0, self._snapshots)
+        if mode == "static":
+            f = filter(lambda x:x.name.find("znap") == -1, self._snapshots)
+        f = list(f)
+        return f
+
+    def snapshot(self, eq, mode="all"):
+        f = self._filter_snapshots(mode)
+        if len(f):
+            return f[eq]
+    
+    def snapshots(self, mode="all"):
+        f = self._filter_snapshots(mode)
+        return f
+
+    def search_snapshot(self, text):
+        res = [x for x in self._snapshots if x.name == text]
+        if len(res):
+            return res[0]
+        else:
+            return False
+
+    def send(self, dataset1, dataset2=None):
+        proc = ["zfs", "send", dataset1.fullname]
+        if dataset2:
+            proc = ["zfs", "send", "-I", dataset1.fullname, dataset2.fullname]
+        proc = self.prefix + proc
+        class Exec:
+            def __init__(self, proc):
+                self.proc = proc
+        return Exec(proc)
+
+    def recv(self):
+        proc = ["zfs", "recv", self.name]
+        proc = self.prefix + proc
+        class Exec:
+            def __init__(self, proc):
+                self.proc = proc
+        return Exec(proc)
+
+    def get(self, attr):
+        proc = ["zfs", "get", attr, self.name]
+        proc = self.prefix + proc
+        o, _ = Popen(proc, stdout=PIPE, stderr=PIPE).communicate()
+        return o.decode().splitlines()[-1].split()[-2]
+
+    def set(self, attr, value):
+        proc = ["zfs", "set", "{}={}".format(attr, value), self.name]
+        proc = self.prefix + proc
+        sys.stderr.write(" ".join(proc) + "\n")
+        o, _ = Popen(proc, stdout=PIPE, stderr=PIPE).communicate()
+        if o:
+            sys.stderr.write(o.decode()+"\n")
 
 
-def reset():
-    for attr in attrs:
-        key = "com.sun:auto-snapshot:" + attr
-        for path in status:
-            if path != "NAME":
-                proc = subprocess.Popen([
-                    "zfs",
-                    "inherit",
-                    key,
-                    path
-                ]).communicate()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="ZFS Auto Snapshot Configure"
+    )
+    parser.add_argument('dataset')
+    parser.add_argument('--on', action="store_true")
+    parser.add_argument('--off', action="store_true")
+    parser.add_argument('--inherit', action="store_true")
+    parser.add_argument('--level', default=None)
+    args = parser.parse_args()
 
+    z = Zfs(args.dataset)
+    bk = "com.sun:auto-snapshot"
+    fks = ["frequent", "hourly", "daily", "weekly", "monthly"]
 
-def set():
-    i = 0
-    for level in args.levels:
-        level = bool(int(level))
-        key = "com.sun:auto-snapshot:" + attrs[i] + "=" + str(level).lower()
-        set = ["zfs", "set", key, args.target]
-        proc = subprocess.Popen(set).communicate()
-        i += 1
+    if args.on:
+        args.level = "11111"
+    if args.off:
+        args.level = "00000"
 
+    if args.level and len(args.level) == 5:
+        for seq, fk in enumerate(fks):
+            v = ["false", "true"][int(args.level[seq])]
+            k = "{}:{}".format(bk, fk)
+            z.set(k, v)
 
-attrs = ["frequent", "hourly", "daily", "weekly", "monthly"]
-status = collections.OrderedDict()
-get = ["zfs", "list"]
-if args.target:
-    get.append("-r")
-    get.append(args.target)
-
-proc = subprocess.Popen(get, stdout=subprocess.PIPE).communicate()
-for line in proc[0].decode("utf-8").splitlines():
-    status[line.split()[0]] = ["0", "0", "0", "0", "0"]
-    if line.split()[0] == "NAME":
-        status[line.split()[0]] = attrs
-
-
-if args.mode == "view":
-    view()
-
-if args.mode == "reset":
-    reset()
-    view()
-
-if args.mode == "on":
-    if not args.levels:
-        args.levels = "11111"
-    if args.target and len(args.levels) == 5:
-        set()
-        view()
-
-if args.mode == "off":
-    args.levels = "00000"
-    if args.target:
-        set()
-        view()
+    sys.stderr.write("\t".join(["NAME"] + fks) + "\n")
+    sys.stderr.write(z.name + "\t")
+    for fk in fks:
+        k = "{}:{}".format(bk, fk)
+        sys.stderr.write(z.get(k) + "\t")
--- zasib.py
+++ zasib.py
@@ -2,140 +2,36 @@
 # -*- coding: utf-8 -*-
 from subprocess import Popen, PIPE
 import argparse
-import sys
-import re
 import os
-import datetime
-
-parser = argparse.ArgumentParser(
-    description="ZFS Auto Snapshot Incremental Backup"
-)
-parser.add_argument('fdataset')
-parser.add_argument('tdataset')
-parser.add_argument('-a', '--all', action="store_true")
-parser.add_argument('-r', '--rename', action="store_true")
-parser.add_argument('-s', '--send', action="store_true")
-parser.add_argument('-c', '--compare', action="store_true")
-parser.add_argument('--revisions', type=int, default=28)
-parser.add_argument('--prefrom', nargs="*", default=[])
-parser.add_argument('--preto', nargs="*", default=[])
-args = parser.parse_args()
-
-
-if "ZASIB_PREF" in os.environ:
-    prefrom = os.environ["ZASIB_PREF"].split()
-if "ZASIB_PRET" in os.environ:
-    preto = os.environ["ZASIB_PRET"].split()
-if args.prefrom:
-    prefrom = args.prefrom
-if args.preto:
-    preto = args.preto
-if args.all:
-    args.rename, args.send, args.compare = True, True, True
-
-class Snapshot:
-    def __init__(self, l, dataset):
-        self.dataset = dataset
-        l = l.split()
-        self.name = l[0]
-        self.name = self.name.split("@")[-1]
-        self.fullname = self.dataset.name + "@" + self.name
-        self.used = l[1]
-        self.avail = l[2]
-        self.refer = l[3]
-        self.mountpoint = l[4]
-
-    def rename(self, newname=None):
-        if newname is None:
-            newname = re.sub(r'[^0-9]', '', self.name)[0:8]
-        newfullname = "{}@{}".format(self.dataset.name, newname)
-
-        if not self.dataset.search_snapshot(newname):
-            sys.stderr.write("# {} => {}\n".format(self.fullname, newfullname))
-            proc = [
-                "zfs",
-                "rename",
-                self.fullname,
-                newname
-            ]
-            proc = self.dataset.prefix + proc
-            sys.stderr.write(" ".join(proc) + "\n")
-            Popen(proc).communicate()
-            self.dataset.create_status()
-    def destroy(self):
-        proc = [
-            "zfs",
-            "destroy",
-            self.fullname
-        ]
-        proc = self.dataset.prefix + proc
-        sys.stderr.write(" ".join(proc) + "\n")
-        Popen(proc).communicate()
-        self.dataset.create_status()
-
-class Zfs:
-    def __init__(self, datasetname):
-        self.name = datasetname
-        self._snapshots = []
-        self.prefix = []
-        self.create_status()
-
-    def create_status(self):
-        proc = ["zfs", "list", self.name, "-t", "snapshot"]
-        o, e = Popen(proc, stdout=PIPE, stderr=PIPE).communicate()
-        self.exists = True
-        if e:
-            self.exists = False
-        for l in o.decode().splitlines():
-            if l.find("NAME") == 0:
-                continue
-            snapshot = Snapshot(l, self)
-            self._snapshots.append(snapshot)
-
-    def _filter_snapshots(self, mode):
-        f = self._snapshots
-        if mode == "znap":
-            f = filter(lambda x:x.name.find("znap") >= 0, self._snapshots)
-        if mode == "static":
-            f = filter(lambda x:x.name.find("znap") == -1, self._snapshots)
-        f = list(f)
-        return f
-
-    def snapshot(self, eq, mode="all"):
-        f = self._filter_snapshots(mode)
-        if len(f):
-            return f[eq]
-    
-    def snapshots(self, mode="all"):
-        f = self._filter_snapshots(mode)
-        return f
-
-    def search_snapshot(self, text):
-        res = [x for x in self._snapshots if x.name == text]
-        if len(res):
-            return res[0]
-        else:
-            return False
-
-    def send(self, dataset1, dataset2=None):
-        proc = ["zfs", "send", dataset1.fullname]
-        if dataset2:
-            proc = ["zfs", "send", "-I", dataset1.fullname, dataset2.fullname]
-        proc = self.prefix + proc
-        class Exec:
-            def __init__(self, proc):
-                self.proc = proc
-        return Exec(proc)
-
-    def recv(self):
-        proc = ["zfs", "recv", self.name]
-        proc = self.prefix + proc
-        class Exec:
-            def __init__(self, proc):
-                self.proc = proc
-        return Exec(proc)
+import sys
+from zasconfig import Zfs, Snapshot
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="ZFS Auto Snapshot Incremental Backup"
+    )
+    parser.add_argument('fdataset')
+    parser.add_argument('tdataset')
+    parser.add_argument('-a', '--all', action="store_true")
+    parser.add_argument('-r', '--rename', action="store_true")
+    parser.add_argument('-s', '--send', action="store_true")
+    parser.add_argument('-c', '--compare', action="store_true")
+    parser.add_argument('--revisions', type=int, default=28)
+    parser.add_argument('--prefrom', nargs="*", default=[])
+    parser.add_argument('--preto', nargs="*", default=[])
+    args = parser.parse_args()
+
+
+    if "ZASIB_PREF" in os.environ:
+        prefrom = os.environ["ZASIB_PREF"].split()
+    if "ZASIB_PRET" in os.environ:
+        preto = os.environ["ZASIB_PRET"].split()
+    if args.prefrom:
+        prefrom = args.prefrom
+    if args.preto:
+        preto = args.preto
+    if args.all:
+        args.rename, args.send, args.compare = True, True, True
     f = Zfs(args.fdataset)
     f.prefix = prefrom
     if f.exists is False:
@@ -177,6 +73,7 @@ if __name__ == '__main__':
                 recv.communicate()
                 f.create_status()
                 t.create_status()
+        t.set("tk.hisaruki.greed:ZASIB_RESTORE", f.name)
 
     if args.compare:
         fa = set([x.name for x in f.snapshots("static")])
