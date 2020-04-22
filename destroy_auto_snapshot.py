cb06a2f02ded17c61427ec66c80941ba1b664716
hisaruki <hisaruki@gmail.com>
2020-04-23 03:58:09 +0900

    destroy_auto_snapshot

--- destroy_auto_snapshot.py
+++ destroy_auto_snapshot.py
@@ -1,42 +1,51 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import subprocess
+from subprocess import Popen, PIPE
 import sys
 import re
 import argparse
-import collections
 
 parser = argparse.ArgumentParser(description="Destroy Auto Snapshot")
 parser.add_argument('target')
+parser.add_argument('--autokey', default="znap")
+parser.add_argument("-r", '--recursive', action="store_true")
 args = parser.parse_args()
 
-proc = subprocess.Popen([
-    "zfs", "list", "-t", "snapshot", "-p"
-], stdout=subprocess.PIPE).communicate()
-
-
-def sizeof_fmt(num, suffix='B'):
+def fmt(num, suffix='B'):
     for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
         if abs(num) < 1024.0:
             return "%3.1f%s%s" % (num, unit, suffix)
         num /= 1024.0
     return "%.1f%s%s" % (num, 'Yi', suffix)
 
-lines = proc[0].decode("utf-8").splitlines()
-lines = map(lambda x: [x.split()[0], x.split()[1]], lines)
-lines = [x for x in lines if re.search(r'znap', x[0])]
+cmd = [
+    "zfs", "list", "-t", "snapshot", "-p"
+]
 if args.target != "all":
-    lines = [x for x in lines if re.search("^" + args.target + "@", x[0])]
-lines = list(map(lambda x: [x[0], int(x[1])], lines))
+    cmd.append(args.target)
+if args.recursive:
+    cmd.append("-r")
+o, e = Popen(cmd, stdout=PIPE).communicate()
+if e:
+    sys.stdout.write(e.decode()+"\n")
+    sys.exit()
 
-gain = sum([x[1] for x in lines])
-
-if len(lines):
-    for line in lines:
-        print(line)
-
-    print(sizeof_fmt(gain) + " free space will be available.")
-    sys.stdout.write("Do you want to continue [Y/n]?")
-    if re.match("y", input().lower()):
-        for line in lines:
-            subprocess.Popen(["zfs", "destroy", line[0]])
+lines = o.decode().splitlines()
+f = filter(lambda x:re.search("@", x), lines)
+f = filter(lambda x:re.search(args.autokey, x), f)
+data = [(line.split()[0], int(line.split()[1]))for line in f]
+for _name, size in data:
+    sys.stdout.write("{}\t{}\n".format(_name, size))
+total = fmt(sum([x[1] for x in data]))
+sys.stdout.write("{} free space will be available.\n".format(total))
+sys.stdout.write("Do you want to continue [y/N]?")
+if re.match("y", input().lower()):
+    for snapshot, _ in data:
+        cmd = ["zfs", "destroy", snapshot]
+        sys.stdout.write("{}\n".format(" ".join(cmd)))
+        o, e = Popen(cmd, stdout=PIPE).communicate()
+        if o:
+            sys.stdout.write(o.decode()+"\n")
+        if e:
+            sys.stdout.write(e.decode()+"\n")
+            sys.exit()
